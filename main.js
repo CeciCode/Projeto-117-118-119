f90053d4ea56c79fb40891093f6b6f0dc7d9f112
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-11-02 15:30:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup() {
+    canvas= createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth= window.speechSynthesis;
+}
+function preload() {
+    classifier= ml5.imageClassifier("DoodleNet");
+}
+function clear() {
+    background("white");
+}
+function draw() {
+    strokeWeight(10);
+    stroke("black");
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    var result= results[0].label;
+    document.getElementById("label").innerHTML= "Nome: " + result.replace("_", " ");
+    document.getElementById("confidence").innerHTML= "Precisão: " + Math.round(results[0].confidence * 100) + "%";
+    utterThis= new SpeechSynthesisUtterance(result.replace("_", " "));
+    synth.speak(utterThis);
+}
